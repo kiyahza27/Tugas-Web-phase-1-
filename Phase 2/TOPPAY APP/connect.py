8c9f6c9f3dbb3c874ff2b50fa8b9ee8399646c60
kiyahza27 <56219654+kiyahza27@users.noreply.github.com>
2020-04-12 23:06:31 +0700

    Delete connect.py

--- Phase 2/TOPPAY APP/connect.py
+++
@@ -1,8 +0,0 @@
-from cassandra.cluster import Cluster
-
-cluster = Cluster(['127.0.0.1'])
-session = cluster.connect()
-
-session.set_keyspace('datauser')
-
-#untuk memnjalankan program ini harus menjalankan cassandra.bat -f nya terlebih dahulu di command prompt(run as admin)
