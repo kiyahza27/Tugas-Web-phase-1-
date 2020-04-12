dd9669505069c6df044b308350e73eb041fb3af1
kiyahza27 <56219654+kiyahza27@users.noreply.github.com>
2020-04-12 12:03:46 +0700

    Update connect.py

--- Phase 2/TOPPAY APP/connect.py
+++ Phase 2/TOPPAY APP/connect.py
@@ -4,3 +4,5 @@ cluster = Cluster(['127.0.0.1'])
 session = cluster.connect()
 
 session.set_keyspace('datauser')
+
+#untuk memnjalankan program ini harus menjalankan cassandra.bat -f nya terlebih dahulu di command prompt(run as admin)
